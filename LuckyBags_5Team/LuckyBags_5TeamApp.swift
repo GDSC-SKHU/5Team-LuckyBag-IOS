10f87d641be620509f239976b646bc2559ec1007
ParkSY0919 <qhr498@gmail.com>
2023-01-23 17:36:28 +0900

    Initial Commit

---
+++ LuckyBags_5Team/LuckyBags_5TeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyBags_5TeamApp.swift
+//  LuckyBags_5Team
+//
+//  Created by 박신영 on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LuckyBags_5TeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
